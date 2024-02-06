c3998f64ec08948b5f6774e967d8c9062b8cbcaa
samthambad <thambad@gmail.com>
2024-02-06 21:00:17 +0800

    rows are created successfully

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,19 +1,40 @@
 'use server';
-
+import { createClient } from '@supabase/supabase-js';
+import { timeStamp } from 'console';
 import { z } from 'zod';
-import {sql} from '@'
+require('dotenv').config()
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+
+if (!supabaseKey || !supabaseUrl) {
+  throw new Error("key or url is missng from env variables!")
+}
+
+const supabase = createClient(supabaseUrl, supabaseKey)
 const PostSchema = z.object({
   id: z.string(),
   title: z.string(),
   description: z.string(),
-  date: z.string(),
+  timestamp: z.string(),
 })
 
-const CreatePost = PostSchema.omit({id:true, date:true})
+const PostAcceptedSchema = PostSchema.omit({id:true})
 
 export async function createPost(formData: FormData) {
-  const { title, description } = CreatePost.parse({
+  const { title, description, timestamp } = PostAcceptedSchema.parse({
     title: formData.get("title"),
     description: formData.get("description"),
+    timestamp: new Date().toISOString(),
   })
+  
+  let {error} = await supabase.from('posts').insert({
+    title: title,
+    description: description,
+    timestamp: timestamp,
+  })
+  console.log(title, description)
+  if(error) {
+    console.log(error);
+  }
 }
--- components/input.tsx
+++ components/input.tsx
@@ -1,10 +1,10 @@
-import { CreatePost } from '@/app/lib/actions'
+import { createPost } from '@/app/lib/actions'
 import React from 'react'
 
 const Input = () => {
   return (
     <div>
-      <form action={CreatePost} className='border border-gray-300 p-4 rounded-lg w-4/5 mx-auto'>
+      <form action={createPost} className='border border-gray-300 p-4 rounded-lg w-4/5 mx-auto'>
         <input className='border border-gray-300 p-2 rounded-md block mb-4 mx-auto' type='text' name="title" placeholder="Enter title..."></input>  
         <textarea className='border border-gray-300 p-2 rounded-md block mb-4 mx-auto w-4/5' name='description' placeholder="Enter description..."></textarea>  
         <input type="submit" placeholder='Submit' className='border border-gray-300 p-2 rounded-md hover:bg-blue-400 mx-auto block' />
