bd3cf0ed72b73726a262100e0931d35bc4897d45
samthambad <thambad@gmail.com>
2024-02-20 17:44:06 +0800

    createpost ui checks whether logged in

--- app/createpost/page.tsx
+++ app/createpost/page.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import "@/styles/globals.css"
 import Input from '@/components/input'
+import { checkLoggedIn } from '@/app/layout';
 
-const CreatePost = () => {
+const CreatePost = async () => {
+  const userDetails = await checkLoggedIn();
   return (
     <div className='text-center'>
-      <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-4'>Create Post</h1>
-      <br className='max-md:hidden mt-4'/>
-      <Input/>
+      {userDetails ?
+      <div>
+        <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-4'>Create Post</h1>
+        <br className='max-md:hidden mt-4'/>
+        <Input/>
+        </div>
+        :
+        <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-4'>You are not logged in!</h1>
+      }
     </div>
   ) 
   }
