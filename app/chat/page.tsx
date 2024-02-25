dc1597c740d9402e9843af2105107672584b0618
samthambad <thambad@gmail.com>
2024-02-25 11:37:02 +0800

    added chat page

---
+++ app/chat/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Chat = () => {
+  return (
+    <div className='flex-center flex-col text-center mx-auto'>
+      <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-8 mx-auto'> Chat </h1>
+      <h1 className='mb-8 border border-gray-300 mx-auto w-80 rounded font-bold'>All Chats</h1>
+    </div>
+  )
+}
+
+export default Chat
