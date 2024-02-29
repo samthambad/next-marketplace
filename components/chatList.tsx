0e3d6085d3b56106762b3be6399406f9ae6dbf55
samthambad <thambad@gmail.com>
2024-02-29 13:16:48 +0800

    can redirect to chat page

--- components/chatList.tsx
+++ components/chatList.tsx
@@ -14,36 +14,40 @@ import {
   TableRow,
 } from "@/components/ui/table"
 import Chat from '../app/chat/page';
+import { redirect, useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 
-async function goToChat(chat_id:number) {
-  console.log("chat_id", chat_id)
-}
 const ChatList = ({data}:{data:Chat[]}) => {
-
+  
+  const router = useRouter()
   // console.log("dataaa", data)
+  async function goToChat(chat_id:number) {
+    console.log("chat_id", chat_id)
+    router.push(`/chat/${chat_id}`)
+  }
   return (
     <div>
       <h1 className='mb-4 border border-gray-300 mx-auto w-80 rounded font-bold'>All Chats</h1>
       <br className='max-md:hidden mt-4 mx-auto'/>
-    <Table className='w-[80%] mx-auto border'>
-      <TableHeader >
-        <TableRow >
-          <TableHead className="w-[20%] text-center">Post Title</TableHead>
-          <TableHead className="w-[20%] text-center">Recent Message</TableHead>
-          <TableHead className="w-[20%] text-center">User</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {data.map((chat:Chat) => (
-          <TableRow key={chat.chat_id} onClick={() => goToChat(chat.chat_id)}>
-            <TableCell className="text-center">{chat.title}</TableCell>
-            <TableCell>{chat.latestMessage}</TableCell>
-            <TableCell className="text-center">{chat.other_name}</TableCell>
+      <Table className='w-[80%] mx-auto border'>
+        <TableHeader >
+          <TableRow >
+            <TableHead className="w-[20%] text-center">Post Title</TableHead>
+            <TableHead className="w-[20%] text-center">Recent Message</TableHead>
+            <TableHead className="w-[20%] text-center">User</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {data.map((chat:Chat) => (
+            <TableRow key={chat.chat_id} onClick={() => goToChat(chat.chat_id)}>
+              <TableCell className="text-center">{chat.title}</TableCell>
+              <TableCell>{chat.latestMessage}</TableCell>
+              <TableCell className="text-center">{chat.other_name}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
     )
   }
