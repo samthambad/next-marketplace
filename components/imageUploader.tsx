774ec7aeb0b4808b01015729c40998a3bc1d35b1
samthambad <thambad@gmail.com>
2024-03-07 17:26:47 +0800

    problem with async compressor

--- components/imageUploader.tsx
+++ components/imageUploader.tsx
@@ -18,54 +18,55 @@ const ImageUploader = () => {
     // convert the files to string and append to newPreviews
     newFiles.forEach((file) => {
       // make sure file is <=10mb
+      console.log("file received")
       if (file.size <= 10 * 1024 * 1024) {
-          // Compress image
-          new Compressor(file, {
-            quality: 0.6,
-            success(result) {
-              newCompressedFiles.push(result as File);
-              newCompressedFiles.forEach((compressedFile) => {
-                const reader = new FileReader();
-                reader.readAsDataURL(compressedFile);
-                // when the file reading operation completes, the below happens
-                reader.onloadend = () => {
-                newPreviews.push(reader.result as string);
-                setPreviews([...previews, ...newPreviews])
-              }
-              
-            })
-            setFiles([...files, ...newCompressedFiles]);
-            },
-            error(err) {
-              console.log('Compression error:', err.message);
-            },
-          });
+        // Compress image
+        new Compressor(file, {
+          quality: 0.6,
+          success(result) {
+            newCompressedFiles.push(result as File);
+          },
+          error(err) {
+            console.log('Compression error:', err.message);
+          },
+        });
       }
       else {
         alert(`File ${file.name} exceeds the size limit of 10MB and will not be uploaded`)
       }
-    })
-    console.log(previews)
-  };
+    });
+    console.log("newcompressedfiles length", newCompressedFiles.length)
+    newCompressedFiles.forEach((compressedFile) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(compressedFile);
+      // when the file reading operation completes, the below happens
+      reader.onloadend = () => {
+        console.log("pushing")
+        newPreviews.push(reader.result as string);
+        setPreviews([...previews, ...newPreviews])
+      };
+    });
+    setFiles([...files, ...newCompressedFiles]);
+  }
   
   return (
-    <div
-    className="drop-area border py-10 border-gray-300 p-4 rounded-lg w-3/5 mx-auto"
+    <div className="drop-area border py-10 border-gray-300 p-4 rounded-lg w-3/5 mx-auto"
     onDrop={handleDrop}
     onDragOver={(e) => e.preventDefault()}
     >
-    <p className='mb-2'><em>Drag and drop your image here</em></p>
+      <p className='mb-2'><em>Drag and drop your image here</em></p>
       {previews.map((preview) => (
-        <div>
+        <div className='mx-auto'>
           <img
             src={preview}
             alt="Preview"
             style={{ maxWidth: '100%', maxHeight: '300px' }}
+            className='mx-auto'
           />
         </div>
       ))}
-        </div>
-        );
+    </div>
+  );
       };
       
       export default ImageUploader;
