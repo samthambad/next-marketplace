2506eb51c39ca4411997be151f12b6021e96773e
sam <thambad@gmail.com>
2024-03-20 09:56:10 +0800

    added postserver again

--- app/profile/[user_id]/page.tsx
+++ app/profile/[user_id]/page.tsx
@@ -19,7 +19,7 @@ const ProfilePage = async ({ params }: { params: { user_id: string } }) => {
       {/* <Image src={userDetails?.user_metadata.picture} alt="profile" width={100} height={100}></Image> */}
       {/* all the users posts */}
       <div className="flex-center flex-col text-center mx-auto m-2">
-        {/* <PostServer /> */}
+        <PostServer />
       </div>
     </div>
   );
