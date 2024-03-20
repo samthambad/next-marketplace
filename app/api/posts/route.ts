846e05c296eab7eade6e7241594e401249a99737
sam <thambad@gmail.com>
2024-03-20 17:55:50 +0800

    removed try-catch for /posts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -2,13 +2,11 @@ import { fetchFilteredPosts } from '../../../lib/actions';
 // call server functions here
 export async function POST(req: any, res: any) {
   const { query } = await req.json();
-  try {
-    console.log("query in route", query)
-    const posts = await fetchFilteredPosts(query);
-    console.log("postss", posts)
+  console.log("query in route", query)
+  const posts = await fetchFilteredPosts(query);
+  console.log("postss", posts)
+  if (posts) {
     return new Response(JSON.stringify(posts));
   }
-  catch (err) {
-    console.log("error in route.ts", err)
-  }
+  else return new Response(JSON.stringify({ status: 400 }))
 }
