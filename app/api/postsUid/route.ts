a4b38253eb69e76c741c07bd761b92b5c3e391a8
sam <thambad@gmail.com>
2024-03-20 17:48:50 +0800

    removed try catch

--- app/api/postsUid/route.ts
+++ app/api/postsUid/route.ts
@@ -1,17 +1,12 @@
 import { checkLoggedIn, fetchFilteredPostsUid } from "@/lib/actions";
 
 export async function GET(req: any, res: any) {
-  try {
-    const userDetails = await checkLoggedIn()
-    const posts = await fetchFilteredPostsUid(userDetails);
-    console.log("postss", posts);
-    if (posts) {
-      return new Response(JSON.stringify(posts));
-    } else {
-      return new Response(JSON.stringify({ status: 200 }))
-    }
-  } catch (err) {
-    console.log("error in route.tsss", err);
+  const userDetails = await checkLoggedIn()
+  const posts = await fetchFilteredPostsUid(userDetails);
+  console.log("postss", posts);
+  if (posts) {
+    return new Response(JSON.stringify(posts));
+  } else {
     return new Response(JSON.stringify({ status: 400 }))
   }
 }
