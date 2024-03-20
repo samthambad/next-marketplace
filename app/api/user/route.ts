f2079b2c71ac9eeacca2a6df6482313b8f76f47a
sam <thambad@gmail.com>
2024-03-20 17:57:57 +0800

    removed try-catch in /user

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -2,18 +2,11 @@ import { checkLoggedIn } from "@/lib/actions";
 
 // call server functions here
 export async function GET(req: any, res: any) {
-  try {
-    const userDetails = await checkLoggedIn();
-    if (userDetails) {
-      const newResponse = new Response(JSON.stringify(userDetails));
-      return newResponse;
-    } else {
-      return new Response(JSON.stringify({ status: 200 }), {
-        headers: { "Content-Type": "application/json" },
-      });
-    }
-  } catch (err) {
-    console.log("error in user route.ts", err);
+  const userDetails = await checkLoggedIn();
+  if (userDetails) {
+    const newResponse = new Response(JSON.stringify(userDetails));
+    return newResponse;
+  } else {
     return new Response(JSON.stringify({ status: 400 }), {
       headers: { "Content-Type": "application/json" },
     });
