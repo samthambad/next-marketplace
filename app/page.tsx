49e2cba7a02b224b79e081d7b309e2d222451a40
sam <thambad@gmail.com>
2024-03-21 17:07:07 +0800

    added title and removing favicon

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 const Home = async () => {
   return (
     <section className='w-full flex-center flex-col text-center'>
+      <title>Marketplace</title>
       <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>Marketplace
         <br className='max-md:hidden' />
       </h1>
