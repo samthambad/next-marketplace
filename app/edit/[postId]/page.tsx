9fd3c6f780ff8ed7d0feb293193baef6582c9746
sam <thambad@gmail.com>
2024-03-24 15:46:15 +0800

    made some visual and funct changes

--- app/edit/[postId]/page.tsx
+++ app/edit/[postId]/page.tsx
@@ -14,20 +14,21 @@ const EditPost = async ({ params }: { params: { postId: string } }) => {
     return (
       <div>
         <form>
+          <span className="mx-2 font-semibold rounded-md border p-1">Title</span>
           <input
             autoComplete="off"
-            className="border border-gray-300 p-2 rounded-md block mb-4 mx-auto"
+            className="border border-gray-300 p-2 rounded-md block mb-2 mx-2 mt-2"
             type="text"
             name="title"
-            value={postData.title}
+            defaultValue={postData.title}
             placeholder="Enter title..."
           ></input>
+          <span className="mx-2 font-semibold rounded-md border p-1">Description</span>
           <textarea
-            className="border border-gray-300 p-2 rounded-md block mb-4 mx-auto w-4/5"
+            className="border border-gray-300 p-2 rounded-md block mt-2 mb-4 w-3/5 mx-2"
             name="description"
-            value={postData.description}
             placeholder="Enter description..."
-          ></textarea>
+          >{postData.description}</textarea>
           <input
             // onClick={() => {
             // add image to db
@@ -37,11 +38,11 @@ const EditPost = async ({ params }: { params: { postId: string } }) => {
             type="submit"
             value="Edit"
             placeholder="Submit"
-            className="border border-gray-300 p-2 rounded-md hover:bg-blue-400 mx-auto block"
+            className="border border-gray-300 text-white hover:bg-blue-700 bg-blue-500 p-2 rounded-md block mb-4 mx-2"
           />
         </form>
         <ImageDisplay image_array={image_array} postId={+params.postId} />
-      </div>
+      </div >
     );
   } else redirect("/");
 };
