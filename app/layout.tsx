9dd80b993dbea6ec42c56485a3ad949d70416cdb
samthambad <thambad@gmail.com>
2024-03-27 16:36:49 +0800

    Added a footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import Navbar from "@/components/ui/navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 import { checkLoggedIn } from '@/lib/actions';
+import Footer from '@/components/footer';
 
 export default async function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
   const userDetails = await checkLoggedIn();
@@ -9,10 +10,13 @@ export default async function RootLayout({children}: Readonly<{children: React.R
     <html lang="en">
       <body> 
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <main>
-            <Navbar user={userDetails} />
-            {children}
-          </main>
+          <div className='flex flex-col min-h-screen'>
+            <main className='flex-grow'>
+              <Navbar user={userDetails} />
+              {children}
+            </main>
+            <Footer/>
+          </div>
         </ThemeProvider>
       </body>
     </html>
---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className='bg-slate-700 text-white py-4 mt-10 bottom-0 text-center w-full'>
+      <div> This marketplace was made by Samarth Thambad</div>
+      <Link href={'mailto:thambad@gmail.com'} className='font-bold hover:text-blue-200'>Contact</Link>
+      <span> | </span>
+      <Link href={'http://github.com/samthambad'} className='font-bold hover:text-blue-200'>GitHub</Link>
+    </footer>
+    )
+  }
+  
+  export default Footer
