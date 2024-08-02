d8c8ca94a8441887492e732f2dff75776a5743d8
sam <thambad@gmail.com>
2024-08-02 11:30:55 +0800

    stop caching endpoint and increase frequency of pinging to 3times/week

--- app/api/ping-db/route.js
+++ app/api/ping-db/route.js
@@ -1,6 +1,10 @@
 import { NextResponse } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
 
+//prevents caching
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL || '', process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY || '')
 
