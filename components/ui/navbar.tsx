9bc5b68eb1e2683a36dbc49bc580a3149dcce2dd
samthambad <thambad@gmail.com>
2024-08-07 11:16:39 +0800

    navbar should be highlighted at its page

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -37,22 +37,23 @@ const Navbar = ({ user }: { user: User | undefined }) => {
     router.push("/");
     router.refresh();
   };
+  console.log("pathname",)
   return (
     <div className="mx-2 flex items-center justify-center gap-[4vw] mb-10 pt-2">
       <Link href="/" className="hover:text-blue-500  font-bold">
         Home
       </Link>
-      <Link href="/search" className={`hover:text-blue-500 font-bold ${pathname.includes("vercel.app/search") ? "text-blue-500" : ""}`}>
+      <Link href="/search" className={`hover:text-blue-500 font-bold ${pathname.includes("/search") ? "text-blue-500" : ""}`}>
         Search
       </Link>
       <div className="hidden sm:gap-[4vw] sm:flex sm:items-center sm:justify-center">
         {user && (
-          <Link href="/createpost" className={`hover:text-blue-500 font-bold ${pathname.includes("vercel.app/createpost") ? "text-blue-500" : ""}`}>
+          <Link href="/createpost" className={`hover:text-blue-500 font-bold ${pathname.includes("/createpost") ? "text-blue-500" : ""}`}>
             Create Post
           </Link>
         )}
         {user && (
-          <Link href="/chat" className={`hover:text-blue-500 font-bold ${pathname.includes("vercel.app/chat") ? "text-blue-500" : ""}`}>
+          <Link href="/chat" className={`hover:text-blue-500 font-bold ${pathname.includes("/chat") ? "text-blue-500" : ""}`}>
             Chats
           </Link>
         )}
